61687e74fae296e9914296c13cf65bb0f26745e2
Gabriel <gabriel.quadra@edu.es.senac.br>
2026-05-14 16:33:30 -0300

    atualiza logica do clique
    - Adiciona saldo dinamico
    - Bloqueia o botao enquanto sorteia os numeros
    - Acrescenta estruta de testes para evitar que o programa feche quando
      um erro acontecer

--- LacoFor/MainWindow.xaml.cs
+++ LacoFor/MainWindow.xaml.cs
@@ -16,29 +16,58 @@ namespace LacoFor;
 /// </summary>
 public partial class MainWindow : Window
 {
+    public decimal saldoInicial = 1234.00M;
+
     public MainWindow()
     {
         InitializeComponent();
+        tbSaldo.Text = $"R$ {saldoInicial}";
     }
 
     private async void BotaoSorteio_OnClick(object sender, RoutedEventArgs e)
     {
         var quantidadeTexto = tbQuantidade.Text;
-        var quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
-        //var quantidadeSorteios = Convert.ToInt32(tbQuantidade.Text);
-
+        int quantidadeSorteios;
+        try
+        {
+            quantidadeSorteios = Convert.ToInt32(quantidadeTexto);
+        }
+        catch (FormatException)
+        {
+            MessageBox.Show("Entrada inválida. Coloque apenas numeros de 1 para cima!");
+            return;
+        }
+        catch (OverflowException)
+        {
+            MessageBox.Show("Erro! O numero digitado é maior que o numero suportado!");
+            return;
+        }
+        
+        // Desabilita o botão
+        btnSorteio.IsEnabled = false;
+        
         if (quantidadeSorteios < 1)
         {
             quantidadeSorteios = 1;
         }
-        
         var sorteador = new Random();
         // contador++ ; contador += 1; contador = contador + 1
         for (int contador = 0; contador < quantidadeSorteios; contador++)
         {
-            tbResultado.Text = sorteador.Next(0, 40000000).ToString();
-            await Task.Delay(1000);
+            if (saldoInicial >= 10)
+            {
+                tbResultado.Text = sorteador.Next(0, 40000000).ToString();
+                saldoInicial -= 10M;
+                tbSaldo.Text = $"R$ {saldoInicial}";
+                await Task.Delay(1000);
+            }
+            else
+            {
+                MessageBox.Show("Você não tem saldo suficiente para realizar o sorteio!");
+                break;
+            }
         }
-        
+        // Habilita o botão
+        btnSorteio.IsEnabled = true;
     }
 }
